71c1036d6af89ac6e653bb176f639fa0c2b5d79b
y_has <yuko1640@gmail.com>
2025-10-04 21:17:15 +0900

    Chatモデル作成

---
+++ src/app/Models/Chat.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+}
