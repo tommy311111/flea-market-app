d04cbed8906d9ac22b58e958e59758f3d0abf6b2
y_has <yuko1640@gmail.com>
2025-10-04 21:16:43 +0900

    Orderモデルに追加機能

--- src/app/Models/Order.php
+++ src/app/Models/Order.php
@@ -24,4 +24,24 @@ class Order extends Model
     }
 
     public const PAYMENT_METHOD = ['コンビニ払い', 'カード支払い'];
+
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
+
+    public function buyer()
+    {
+        return $this->belongsTo(User::class, 'buyer_id');
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
