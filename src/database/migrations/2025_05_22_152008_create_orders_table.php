326d1b8d99f1086dbdea76e6f240e41fcd088459
y_has <yuko1640@gmail.com>
2025-10-05 14:43:32 +0900

    テーブルにカラム追加

--- src/database/migrations/2025_05_22_152008_create_orders_table.php
+++ src/database/migrations/2025_05_22_152008_create_orders_table.php
@@ -6,19 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateOrdersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('buyer_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('seller_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('item_id')->constrained()->onDelete('cascade');
-            $table->enum('payment_method', ['コンビニ払い','カード支払い']);
+            $table->enum('payment_method', ['コンビニ払い', 'カード支払い']);
+            $table->enum('status', ['pending', 'completed'])->default('pending');
             $table->string('sending_postcode', 8);
             $table->string('sending_address');
             $table->string('sending_building');
@@ -26,12 +22,7 @@ class CreateOrdersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('orders');
     }
--- src/database/migrations/2025_09_29_140253_create_chats_table.php
+++ src/database/migrations/2025_09_29_140253_create_chats_table.php
@@ -6,34 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateChatsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-
     public function up(): void
     {
         Schema::create('chats', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')
                   ->constrained()
-                  ->onDelete('cascade'); // 取引削除時に関連チャットも削除
+                  ->onDelete('cascade');
             $table->foreignId('sender_id')
                   ->constrained('users')
-                  ->onDelete('cascade'); // ユーザー削除時に関連チャットも削除
-            $table->text('message'); // 必須（バリデーションで400文字まで制御）
-            $table->string('image')->nullable(); // 任意（添付画像）
+                  ->onDelete('cascade');
+            $table->text('message');
+            $table->string('image')->nullable();
+            $table->boolean('is_read')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('chats');
     }
